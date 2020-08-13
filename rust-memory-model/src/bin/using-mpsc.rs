654ee60944e3acb54ec23299e6a2bd9d8743fb87
James Hagans <jhagans@acm.org>
2020-08-12 22:25:20 -0400

    add mspc code

---
+++ rust-memory-model/src/bin/using-mpsc.rs
@@ -0,0 +1,34 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn writer(chan: mpsc::SyncSender<u32>) -> () {
+    let mut cur: u32 = 0;
+    while let Ok(()) = chan.send(cur) {
+        cur = cur.wrapping_add(1);
+    }
+}
+
+fn reader(read_limit: usize, chan: mpsc::Receiver<u32>) -> () {
+    let mut cur: u32 = 0;
+    while (cur as usize) < read_limit {
+        let num = chan.recv().unwrap();
+        assert_eq!(num, cur);
+        cur = cur.wrapping_add(1);
+    }
+}
+
+// cargo run --bin using-mpsc
+fn main() {
+    let capacity = 10;
+    let read_limit = 1_000_000;
+    let (snd, rcv) = mpsc::sync_channel(capacity);
+
+    let reader_jh = thread::spawn(move || {
+        reader(read_limit, rcv);
+    });
+    let _writer_jh = thread::spawn(move || {
+        writer(snd);
+    });
+
+    reader_jh.join().unwrap();
+}
